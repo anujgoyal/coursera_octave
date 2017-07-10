1e6e5b0d0a66b6ba5676e4734d22db9acfdfcc49
Anuj Goyal <anuj.goyal@gmail.com>
2017-07-09 23:55:39 -0700

    predict, bin to either 1 or 0.

--- machine-learning-ex2/ex2/predict.m
+++ machine-learning-ex2/ex2/predict.m
@@ -13,13 +13,16 @@ p = zeros(m, 1);
 % Instructions: Complete the following code to make predictions using
 %               your learned logistic regression parameters. 
 %               You should set p to a vector of 0's and 1's
-%
 
+p = sigmoid(X*theta);
 
-
-
-
-
+for i = 1:length(p)
+    if p(i) >= 0.5
+        p(i) = 1;
+    else
+        p(i) = 0;
+    end
+end
 
 % =========================================================================
 
