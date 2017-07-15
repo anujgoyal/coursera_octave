4a12e44e3bb4aa90385106a62f6d235ab5fcad50
Anuj Goyal <anuj.goyal@gmail.com>
2017-07-15 01:06:02 -0700

    prediction using a neural network, make sure to simulate each activation layer

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,12 +21,21 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% add 1 for bias
+X = [ones(m,1) X];
+%asup1, inputs or X in this case
+asup2 = sigmoid(X * Theta1');   % (5000 x 401) X (401 x 25)
 
+%size (X)
+%size (Theta1)
+%size (asup2)
 
+% add 1 for bias
+asup2 = [ones(size(asup2,1), 1) asup2];
+asup3 = sigmoid(asup2 * Theta2');   % (5000 x 26) X (26 x 10)
 
-
-
-
+% select index with max value
+[val, p]  = max(asup3, [], 2);
 
 
 % =========================================================================
