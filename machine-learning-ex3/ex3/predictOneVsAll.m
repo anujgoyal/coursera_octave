1dfa1f665d2196733a002b663e230e6453b76c18
Anuj Goyal <anuj.goyal@gmail.com>
2017-07-15 00:35:46 -0700

    use calculated thetas to predict handwriting samples

--- machine-learning-ex3/ex3/predictOneVsAll.m
+++ machine-learning-ex3/ex3/predictOneVsAll.m
@@ -29,12 +29,11 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
+p = sigmoid(X*all_theta');
 
-
-
-
-
-
+% find max index for each row
+% each index corresponds to each label
+[val, p] = max(p, [], 2); % find max index for each row
 
 % =========================================================================
 
